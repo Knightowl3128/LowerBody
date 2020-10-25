--fixed bug (zmp calculation) --fixed bug (index rounding error)

Online Controller/lqr.py
@@ -1,5 +1,5 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy import linalg as sci
 from scipy.optimize import fsolve
 
@@ -61,7 +61,7 @@ X[0, 0, 0] = 0.09
 # print(X)
 # print(K)
 p_actual = np.zeros((1, N))
-
+UHistory = np.zeros((1, N))
 for index, val in enumerate(t):
     if index == N - 1:
         break
@@ -69,11 +69,12 @@ for index, val in enumerate(t):
     X[index + 1] = A @ X[index] + B @ u_k
 
     p_actual[0, index] = C @ X[index]
-
+    UHistory[0, index] = u_k
 # print(X[:,0])
 plt.plot(t, p_ref_y[0:360], label='plan zmp')
 plt.plot(t[0:360], X[:, 0], label='CoM Pos')
 # plt.plot(t[0:360],X[:,1],label = 'CoM Vel')
 plt.plot(t[0:360], p_actual.T, label='actual zmp')
+plt.plot(t[0:360], UHistory.T, label='actual zmp')
 plt.legend()
 plt.show()

Online Controller/mpc.py
@@ -1,19 +1,23 @@
-import do_mpc
-from casadi import *
-import matplotlib.pyplot as plt
-import sys, os
+import os
+import sys
 import time
+
+import do_mpc
+import matplotlib.pyplot as plt
+from casadi import *
 
 T_supp = 0.6
 T_dbl = 0.08
 speed = 0.1
-zc = 0.6
+zc = 0.46
 g = 9.81
-dt = 0.01
-steps = 16
+dt = 0.005
+steps = 10
 N = int(T_supp * steps / dt + 1)
-N_Horizon = 180
+N_Horizon = 200
+step_size = 0.1
 
+# p_ref_y------
 top = [0.18 for x in range(int(T_supp / dt))]
 bot = [0 for x in range(int(T_supp / dt))]
 
@@ -26,8 +30,25 @@ dsp_2.reverse()
 p_ref_y = [0.09 for x in range(int(T_supp / dt))]
 for i in range(int(steps)):
     p_ref_y = p_ref_y + top + dsp_1 + bot + dsp_2
-
 p_ref_y = np.array(p_ref_y).reshape((len(p_ref_y), 1))
+
+# p_ref_x ---------
+
+ssp_ones = np.ones((len(top)))
+
+dsp_x = np.array([(step_size) * t / T_dbl for t in t_dsp])
+
+p_ref_x = np.array([])
+for step in range(int(steps)):
+    if step == 0:
+        p_ref_x = np.append(p_ref_x, step_size * step * ssp_ones)
+        continue
+    else:
+        foo = np.concatenate((np.add(dsp_x, (step - 1) * step_size), step_size * step * ssp_ones), axis=0)
+        p_ref_x = np.append(p_ref_x, foo)
+        continue
+p_ref_x = np.array(p_ref_x).reshape((len(p_ref_x), 1))
+
 A = np.array([[1, dt, dt ** 2 / 2],
               [0, 1, dt],
               [0, 0, 1]])
@@ -67,7 +88,7 @@ setup_mpc = {
     'state_discretization': 'discrete',
     'store_full_solution': False,
     # Use MA27 linear solver in ipopt for faster calculations:
-    'nlpsol_opts': {'ipopt.linear_solver': 'MA27'}
+    # 'nlpsol_opts': {'ipopt.linear_solver': 'MA27'}
 }
 
 mpc.set_param(**setup_mpc)
@@ -75,19 +96,22 @@ mpc.set_param(**setup_mpc)
 # In[9]:
 
 
-lterm = 1 * (model.x['y'] - model.tvp['p_ref']) ** 2
-mterm = (model.x['y'] - p_ref_y[N_Horizon, 0]) ** 2
+lterm = 100 * (model.x['y'] - model.tvp['p_ref']) ** 2
+mterm = 1 * (model.x['y'] - p_ref_y[N_Horizon, 0]) ** 2
 mpc.set_objective(mterm=mterm, lterm=lterm)
 mpc.set_rterm(u=1e-8)  # input penalty
-
+print(p_ref)
 # In[10]:
 
 
-mpc.bounds['lower', '_x', 'x'] = np.array([[-0.00], [-5], [-5]])
-mpc.bounds['upper', '_x', 'x'] = np.array([[0.18], [5], [5]])
+mpc.bounds['lower', '_x', 'x'] = np.array([[-0.00], [-10], [-15]])
+mpc.bounds['upper', '_x', 'x'] = np.array([[0.19], [10], [10]])
 
 mpc.bounds['lower', '_x', 'y'] = np.array([[0]])
-mpc.bounds['upper', '_x', 'y'] = -np.array([[-0.18]])
+mpc.bounds['upper', '_x', 'y'] = np.array([[0.2]])
+
+mpc.bounds['lower', '_u', 'u'] = np.array([[-300]])
+mpc.bounds['upper', '_u', 'u'] = np.array([[300]])
 
 tvp_template = mpc.get_tvp_template()
 
@@ -129,19 +153,24 @@ mpc.set_initial_guess()
 time_now = time.time()
 sys.stdout = open(os.devnull, "w")
 
-for k in range(N_Horizon):
+for k in range(3000):
+    mpc.lb_opt_x
     u0 = mpc.make_step(x0)
     y_next = simulator.make_step(u0)
     x0 = estimator.make_step(y_next)
 
+with open('test.npy', 'wb') as f:
+    np.save(f, mpc.data['_x'])
+
 sys.stdout = sys.__stdout__
 print(time.time() - time_now)
-plt.plot(mpc.data['_time'], mpc.data['_x'][:, 0])
-plt.plot(mpc.data['_time'], mpc.data['_x'][:, 3])
-plt.plot(mpc.data['_time'], p_ref_y[:len(mpc.data['_time']), 0])
-
+plt.plot(mpc.data['_time'], mpc.data['_x'][:, 0], label='x[0]')
+plt.plot(mpc.data['_time'], mpc.data['_x'][:, 3], label='y')
+plt.plot(mpc.data['_time'], p_ref_y[:len(mpc.data['_time']), 0], label='y_ref')
 # In[18]:
-
+plt.legend()
+plt.figure()
+plt.plot(mpc.data['_time'], mpc.data['_u'][:, 0])
 
 plt.show()
 # In[19]:

Robot.py
@@ -1,11 +1,12 @@
 from numpy import *
-from Link import *
 from scipy.interpolate import CubicSpline
+
+from Link import *
+
+
 # import rospy
 # from std_msgs.msg import Float64
 # from control_msgs.msg import JointControllerState
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
 
 
 class Robot(Link):
@@ -204,17 +205,25 @@ class Robot(Link):
             w = zeros((3, 1))  # angular velocity in 3x1 matrix
             alpha = zeros((3, 1))  # angular acceleration in 3x1 matrix
             for index, val in enumerate(listnow):
+                # print(val.name,val.com_pos,val.com_vel)
                 P += val.mass * val.com_vel
                 w = w + listnow[index - 1].trans_mat[0:3, 0:3] * (val.dq * val.a.T)
                 R = listnow[index].trans_mat[0:3, 0:3]
                 L = L + cross(val.com_pos, (val.mass * val.com_vel)).T + (R * val.I * R.T) * w
-
+            listnow = self.links_r
+        # print(self.links_l[3].end,self.links_r[3].end)
         self.dL = (L - self.L) / self.time_step
         self.dP = (P - self.P) / self.time_step
+        # print('this is p', P)
+        # if abs(self.dP[0,1])>=10:
+        #
+        #     print('----------------------------------------------------------------------------')
+        #     print('this is p', P)
+        #     print(self.links_l[3].end,self.links_r[3].end)
+        #     print('000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000')
         self.L = L
         self.P = P
         return L, P
-        pass
 
     def find_zmp(self):
 
@@ -225,6 +234,7 @@ class Robot(Link):
         # print(total)
         # dH = array([[0], [0], [0]])
         # dP = array([[0, 0, 0]])
+
         x_zmp = (total * 9.81 * pos[0, 0] - dH[1, 0]) / (total * 9.81 + dP[0, 2])
         y_zmp = (total * 9.81 * pos[0, 1] + dH[0, 0]) / (total * 9.81 + dP[0, 2])  # TODO fix this dH its wrong
         return x_zmp, y_zmp